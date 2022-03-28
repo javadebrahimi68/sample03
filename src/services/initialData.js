7f6dc9618108bf7cd34aec741d200d6128ee026d
javadebrahimi68 <javad2.ebrahimi@gmail.com>
2022-03-28 11:03:56 +0430

    Create books  initial Data

---
+++ src/services/initialData.js
@@ -0,0 +1,6 @@
+const books=[
+    {id:1 , title:'The Grass is Always Greener' , author:'Jeffrey Archer' ,cat:'Modern Times'},
+    {id:2 , title:'The Open Boat' , author:'Stephen Crane (1871-1900)' ,cat:'Classic'},
+    {id:3 , title:'The Five Orange Pips' , author:'Arthur Conan Doyle' ,cat:'Crime'},
+    {id:4 , title:'The Judge House' , author:'Bram Stoker (1847-1912)' ,cat:'Suspense'}
+]

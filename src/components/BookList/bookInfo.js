ff573f80f959ef1a375d2c26ee2a75d2ffde9aac
javadebrahimi68 <javad2.ebrahimi@gmail.com>
2022-03-28 16:03:11 +0430

    book info icon
    change icon

--- src/components/BookList/bookInfo.js
+++ src/components/BookList/bookInfo.js
@@ -1,44 +1,45 @@
 import React from 'react'
 
-export const bookInfo = ({books,editBook,removeBook}) => {
- 
-  return (
-    <div className="col-md-6">
-    <div className="card card-stats">
-        <div className="card-body ">
-            <table className='table table-bordered table-striped'>
-                <thead className='text-primary'>
-                    <tr>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Category</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
+export const bookInfo = ({ books, editBook, removeBook }) => {
 
-                        books.map((item) => {
-                            return (
-                                <tr key={item.id}>
-                                    <td>{item.id}</td>
-                                    <td>{item.title}</td>
-                                    <td>{item.author}</td>
-                                    <td>{item.cat}</td>
-                                    <td>
-                                        <button className='btn btn-success mr-1' onClick={()=>editBook(item.id)}>Edit</button>
-                                        <button className='btn btn-danger mr-1'onClick={()=>removeBook(item.id)}>Remove</button>
-                                    </td>
-                                </tr>)
-                        })
-                    }
-                </tbody>
-            </table>
+    return (
+        <div className="col-md-6">
+            <div className="card card-stats">
+                <div className="card-body ">
+                    <table className='table table-bordered table-striped'>
+                        <thead className='text-primary'>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Category</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+
+                                books.map((item) => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td>{item.title}</td>
+                                            <td>{item.author}</td>
+                                            <td>{item.cat}</td>
+                                            <td>
+                                                <button className='btn btn-success mr-1' onClick={() => editBook(item.id)}>      <i className= "nc-icon nc-caps-small mr-1"></i>Edit</button>
+                                                <button className='btn btn-danger mr-1 ' onClick={() => removeBook(item.id)}> <i className=" nc-icon nc-simple-remove mr-1"></i>Remove</button>
+
+                                            </td>
+                                        </tr>)
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
-
-    </div>
-</div>
-  )
+    )
 }
 export default bookInfo;
